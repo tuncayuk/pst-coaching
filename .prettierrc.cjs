94965df1fc1127c0cf333a7d55792b53c58596fa
tuncayyildirtanvadi <tuncay.yildirtan@vadi.com.tr>
2026-04-05 23:13:32 +0100

    chore: standardize quotes in Prettier configuration

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,15 +1,14 @@
 module.exports = {
-  // singleQuote: false,
   // trailingComma: "es5",
   semi: true,
-  trailingComma: 'none',
+  trailingComma: "none",
   singleQuote: true,
   printWidth: 120,
-  arrowParens: 'avoid',
+  arrowParens: "avoid",
   tabWidth: 2,
-  endOfLine: 'lf',
-  importOrder: ['^[./]'],
+  endOfLine: "lf",
+  importOrder: ["^[./]"],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
-  plugins: ['@trivago/prettier-plugin-sort-imports']
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
 };
