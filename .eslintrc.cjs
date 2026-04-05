833bdb00f4dae6b4dacbd7c1545edf1f426be00f
tuncayyildirtanvadi <tuncay.yildirtan@vadi.com.tr>
2026-04-05 20:22:25 +0100

    fix: Standardize quotes in ESLint configuration and update VSCode settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,24 +4,24 @@ module.exports = {
     es2022: true,
     node: true
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2022,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true
     }
   },
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
-  extends: ["expo", "prettier"],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: ['expo', 'prettier'],
   settings: {
     react: {
-      version: "detect"
+      version: 'detect'
     }
   },
-  ignorePatterns: ["**/node_modules/**", "**/dist/**", "**/coverage/**", "**/.expo/**"],
+  ignorePatterns: ['**/node_modules/**', '**/dist/**', '**/coverage/**', '**/.expo/**'],
   rules: {
     // @typescript-eslint/ban-types was removed in v8; disable to avoid config-expo conflict
-    "@typescript-eslint/ban-types": "off"
+    '@typescript-eslint/ban-types': 'off'
   }
 };
