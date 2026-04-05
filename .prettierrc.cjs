07292322730e554d9531bdb3cd052df59243ae50
tuncayyildirtanvadi <tuncay.yildirtan@vadi.com.tr>
2026-04-05 20:23:01 +0100

    feat: Update Prettier configuration and add sorting plugin

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,15 @@
 module.exports = {
-  singleQuote: false,
-  trailingComma: "es5",
-  printWidth: 100,
-  semi: true
+  // singleQuote: false,
+  // trailingComma: "es5",
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 120,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  endOfLine: 'lf',
+  importOrder: ['^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports']
 };
